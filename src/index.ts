e33136d1f6c6d9234fd4fdb10bef8b61770c224e
Dmitry16 <europartner@inbox.ru>
2019-08-19 11:57:33 +0300

    ts_errors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 // cmd + shft + p -> typescript restart server
 
-import { generate } from 'shortid';
+import express from 'express';
 
-console.log('zzzzzzzeeeeeaaaaa!');
+const add = (a: number, b?: number) => {
+  // a way to get rid of the ts error (the red squiggling line):
+  // if (b) {
+  //   return a + b;
+  // } else {
+  //   return a;
+  // }
+  // another way to solve the ts error:
+  // return a + (b as number);
+  // and one more:
+  return a + b!;
+}
+
+const app = express();
+
+app.get('/', (req: any) => {
+  add(1);
+})
+
+app.listen(3000, () => {
+  console.log('started!');
+});
